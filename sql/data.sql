8f300e437d9393566f1ece0675f707d3e17957ee
Pedro Leite <pedrohleite2005@gmail.com>
2025-08-14 18:09:46 -0300

    feat(schema): criação da tabela seguidores

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,7 @@
-CREATE DATABASE IF NOT EXISTS devbook;
-USE devbook;
+    CREATE DATABASE IF NOT EXISTS devbook;
+    USE devbook;
 
+DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
@@ -11,3 +12,28 @@ CREATE TABLE usuarios(
     senha varchar(100) not null,
     criadoEm timestamp default current_timestamp()
 );
+
+CREATE TABLE seguidores(
+    usuario_id int not null,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    seguidor_id int not null,
+    FOREIGN KEY (seguidor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    primary key (usuario_id, seguidor_id)
+);
+
+
+insert into usuarios(nome, nick, email, senha)
+values
+("Pedrinho Matador", "pedrinho", "pedrinho@gmail.com", "$2a$10$yB0Ap1HM/Bl3RkrpzmncRe6MzeVKf5csYhFEe5b4VBum9vqI38I3K"),
+("Lazaro Matador", "lazaro", "lazaro@gmail.com", "$2a$10$2wBcmZZKdUU.ryhve04uMuKuUns.qrFb5nA0V13Pb4k8u4gpgmHTG"),
+("Mock User", "mock", "mock@gmail.com", "$2a$10$mtxH87HOLtYcZkiSJFk7Re4PwJaOXiqBUuc9b.jmt.1.tHSWHq9ny");
+
+insert into seguidores(usuario_id, seguidor_id)
+values
+(1, 2),
+(3, 1),
+(1, 3);
