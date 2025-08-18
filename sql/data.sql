75acb97f52d62d787cc17a8f1d2bd444a49209c8
Pedro Leite <pedrohleite2005@gmail.com>
2025-08-18 17:40:18 -0300

    feat: criação da tabela e model de publicação

--- sql/data.sql
+++ sql/data.sql
@@ -1,6 +1,7 @@
     CREATE DATABASE IF NOT EXISTS devbook;
     USE devbook;
 
+DROP TABLE IF EXISTS publicacoes;
 DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
 
@@ -23,6 +24,18 @@ CREATE TABLE seguidores(
     REFERENCES usuarios(id)
     ON DELETE CASCADE,
     primary key (usuario_id, seguidor_id)
+);
+
+CREATE TABLE publicacoes(
+    id int auto_increment primary key,
+    titulo varchar(50) not null,
+    conteudo varchar(300) not null,
+    autor_id int not null,
+    FOREIGN KEY (autor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+    curtidas int default 0,
+    criadaEm timestamp default current_timestamp
 );
 
 
