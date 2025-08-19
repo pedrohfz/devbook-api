acbba838836cf5df633469b6f4389b13bfb99798
Pedro Leite <pedrohleite2005@gmail.com>
2025-08-19 20:19:45 -0300

    feat(db): edição do script inicial do banco devbook

--- sql/data.sql
+++ sql/data.sql
@@ -38,21 +38,24 @@ CREATE TABLE publicacoes(
     criadaEm timestamp default current_timestamp
 );
 
-insert into usuarios(nome, nick, email, senha)
-values
-("Pedrinho", "pedrinho", "pedrinho@gmail.com", "$2a$10$yB0Ap1HM/Bl3RkrpzmncRe6MzeVKf5csYhFEe5b4VBum9vqI38I3K"),
-("Julia", "julia", "julia@gmail.com", "$2a$10$2wBcmZZKdUU.ryhve04uMuKuUns.qrFb5nA0V13Pb4k8u4gpgmHTG"),
-("Mock User", "mock", "mock@gmail.com", "$2a$10$mtxH87HOLtYcZkiSJFk7Re4PwJaOXiqBUuc9b.jmt.1.tHSWHq9ny");
- $2a$10$GsXIMenXrfDzqBWG/o5NDOlrxVBA7iBE2./VJLo38z1u5tp6Sk1uG
+INSERT INTO usuarios(nome, nick, email, senha) VALUES
+("Pedro", "pedro", "pedro@gmail.com", "$2a$10$yB0Ap1HM/Bl3RkrpzmncRe6MzeVKf5csYhFEe5b4VBum9vqI38I3K"),
+("Marcelo", "marcelo", "marcelo@gmail.com", "$2a$10$2wBcmZZKdUU.ryhve04uMuKuUns.qrFb5nA0V13Pb4k8u4gpgmHTG"),
+("Gabriel", "gabriel", "gabriel@gmail.com", "$2a$10$mtxH87HOLtYcZkiSJFk7Re4PwJaOXiqBUuc9b.jmt.1.tHSWHq9ny"),
+("Matheus", "matheus", "matheus@gmail.com", "$2a$10$GsXIMenXrfDzqBWG/o5NDOlrxVBA7iBE2./VJLo38z1u5tp6Sk1uG");
 
-insert into seguidores(usuario_id, seguidor_id)
-values
+INSERT INTO seguidores(usuario_id, seguidor_id) VALUES
 (1, 2),
 (3, 1),
-(1, 3);
+(1, 4),
+(2, 3),
+(3, 2),
+(2, 4),
+(4, 3),
+(4, 1);
 
-insert into publicacoes(titulo, conteudo, autor_id)
-values
-("Primeira Publicação", "Hello, World! My name is Pedrinho!", 1),
-("Segunda Publicação", "Hello, World! My name is Julia!", 2),
-("Terceira Publicação", "Hello, World! My name is Mock User!", 3);
+INSERT INTO publicacoes(titulo, conteudo, autor_id) VALUES
+("Primeira Publicação do Blog!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 1),
+("Segunda Publicação do Blog!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 2),
+("Terceira Publicação do Blog!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 3),
+("Quarta Publicação do Blog!", "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 4);
